e7e20663031940e9f88816454d9c6152982b7381
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2018-06-02 22:17:36 +0700

    Menambahkan Fragment HomeFragment

--- app/src/main/java/com/ithb/jeffry/myflexiblefragment/MainActivity.java
+++ app/src/main/java/com/ithb/jeffry/myflexiblefragment/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ithb.jeffry.myflexiblefragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+
+        HomeFragment mHomeFragment = new HomeFragment();
+
+        mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+
+        Log.d("MyFlexibleFragment", "Fragment Name: " + HomeFragment.class.getSimpleName());
+        mFragmentTransaction.commit();
     }
 }
